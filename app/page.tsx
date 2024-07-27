cab9a17b23bfbaf4c0dbdbe2f88e43023850ef39
Abdullah-Jammal <129687747+Abdullah-Jammal@users.noreply.github.com>
2024-07-27 15:46:51 +0300

    tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="mt-6 flex items-center justify-center text-3xl">
-      <h1>Welcome DevFlow (New Web)</h1>
+    <div className="flex-center">
+      <h1 className="h1-bold">Welcome DevFlow (New Web)</h1>
     </div>
   );
 }
