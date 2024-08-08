0a5c5016df066ed4e80aee47fe97e2b185dd4ae4
Abdullah-Jammal <129687747+Abdullah-Jammal@users.noreply.github.com>
2024-08-08 21:42:15 +0300

    publicRoute

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,23 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/ask-question(.*)"]);
+const isPublicRoute = createRouteMatcher(
+    [
+        '/',
+        '/api/webhook',
+        '/question/:id',
+        '/tags',
+        '/tags/:id',
+        '/profile/:id',
+        '/comunity',
+        '/jobs'
+    ]
+)
 
 export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) auth().protect();
+    if (isProtectedRoute(req) && !isPublicRoute(req)) {
+        auth().protect();
+    }
 });
 
 export const config = {
