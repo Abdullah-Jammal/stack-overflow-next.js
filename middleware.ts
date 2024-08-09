206c08cb65d61e2d589bb35b83eda48379d66f1a
Abdullah-Jammal <129687747+Abdullah-Jammal@users.noreply.github.com>
2024-08-09 15:45:51 +0300

    ignore route

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,29 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+// Define the protected, public, and ignored routes
 const isProtectedRoute = createRouteMatcher(["/ask-question(.*)"]);
-const isPublicRoute = createRouteMatcher(
-    [
-        '/',
-        '/api/webhook',
-        '/question/:id',
-        '/tags',
-        '/tags/:id',
-        '/profile/:id',
-        '/comunity',
-        '/jobs'
-    ]
-)
+const isPublicRoute = createRouteMatcher([
+    '/',
+    '/api/webhook',
+    '/question/:id',
+    '/tags',
+    '/tags/:id',
+    '/profile/:id',
+    '/comunity',
+    '/jobs'
+]);
+const isIgnoredRoute = createRouteMatcher([
+    '/api/webhook',
+    '/api/chatgpt',
+]);
 
 export default clerkMiddleware((auth, req) => {
+    // Check if the route should be ignored
+    if (isIgnoredRoute(req)) {
+        return; // Skip middleware for ignored routes
+    }
+
+    // Protect route if it's protected and not public
     if (isProtectedRoute(req) && !isPublicRoute(req)) {
         auth().protect();
     }
